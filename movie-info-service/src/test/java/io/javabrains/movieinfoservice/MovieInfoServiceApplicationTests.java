2491bc66cd64b8f7015ca856a498209f9b8ca09f
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2021-10-20 19:54:04 +0530

    Configured Movie Info Service as Eureka-Client

--- movie-info-service/src/test/java/io/javabrains/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/io/javabrains/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package io.javabrains.movieinfoservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableEurekaClient
 public class MovieInfoServiceApplicationTests {
 
 	@Test
